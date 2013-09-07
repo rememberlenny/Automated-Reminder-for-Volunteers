cf5fa658ec8f28ea7c4366bd5218ccfa36a39267
rememberlenny <lkbgift@gmail.com>
2013-09-07 09:46:49 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gajokai::Application.config.secret_token = 'a9ac62134f301c0814b71e8764175425e7232e5fb0bffa2d6e78f65cb90827408baacfde4b7640c93b5376b975955d928526a1ac3c836a28d573a2537e348086'
